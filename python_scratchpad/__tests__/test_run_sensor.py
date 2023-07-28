6ae033b0194d929a11370559a2e6a419743a76b7
Nicholas Ferrara <nferrara100@gmail.com>
2023-07-28 13:07:53 +0100

    feat: live

---
+++ python_scratchpad/__tests__/test_run_sensor.py
@@ -0,0 +1,18 @@
+from unittest.mock import patch
+from ..run_sensor import send_state
+import requests
+
+
+@patch("requests.post")
+def test_send_state_success(post_mock):
+    response = send_state({"id": 1})
+    assert response == True
+    post_mock.assert_called_once_with("https://en6msadu8lecg.x.pipedream.net/", json={"id": 1})
+
+
+@patch("requests.post")
+def test_send_state_fail(post_mock):
+    post_mock.side_effect = requests.RequestException()
+    response = send_state({"id": 1})
+    assert response == False
+    post_mock.assert_called_once_with("https://en6msadu8lecg.x.pipedream.net/", json={"id": 1})
--- python_scratchpad/run_sensor.py
+++ python_scratchpad/run_sensor.py
@@ -2,14 +2,14 @@
 ## Overview ##
 
 You’re designing software for a mission-critical device which contains multiple sensors, each controlled by an independent program.
-Each of these sensor programs is able to generate messages describing its state. It is key that the device be able to report this information 
-to a centralised cloud-based monitoring system, so that faults and downtime can be minimised. To accomplish this you’ve been tasked with 
-designing software that will enable each of these programs to report their state to the cloud monitoring system. This file contains the template 
-to use to emulate a sensor program. The device is running a Linux environment. You may consider using any python libraries and Linux utilities. 
+Each of these sensor programs is able to generate messages describing its state. It is key that the device be able to report this information
+to a centralised cloud-based monitoring system, so that faults and downtime can be minimised. To accomplish this you’ve been tasked with
+designing software that will enable each of these programs to report their state to the cloud monitoring system. This file contains the template
+to use to emulate a sensor program. The device is running a Linux environment. You may consider using any python libraries and Linux utilities.
 
 ## Objective ##
 
-Your task is to design the __send_state__ function and any additional software needed to achieve the desired functionality, respecting the 
+Your task is to design the __send_state__ function and any additional software needed to achieve the desired functionality, respecting the
 constraints below. The resulting work is expected to be runnable and demonstrate the desired functionality.
 
 ## Constraints ##
@@ -18,20 +18,21 @@ constraints below. The resulting work is expected to be runnable and demonstrate
 
 2. Unit tests should be present for the send_state function.
 
-3. The sensor data processing function is mission-critical and time sensitive: calls to the __send_state__ function should be non-blocking with 
+3. The sensor data processing function is mission-critical and time sensitive: calls to the __send_state__ function should be non-blocking with
 minimal performance impact regardless of connectivity issues.
 
-4. The device should be able to run in offline mode, in which case any connectivity requests will fail. In this case the desired behavior is 
-that the state events should be buffered until the device comes online, at which point these events would be sent. Consider edge cases for when 
+4. The device should be able to run in offline mode, in which case any connectivity requests will fail. In this case the desired behavior is
+that the state events should be buffered until the device comes online, at which point these events would be sent. Consider edge cases for when
 the device runs offline for extended periods of time.
 
-5. BONUS: Consider that the device can be turned off and turned on at any time during its normal use. Implement an approach that allows the device 
+5. BONUS: Consider that the device can be turned off and turned on at any time during its normal use. Implement an approach that allows the device
 to not lose buffered events in case of a poweroff.
 """
 
 import time
 import uuid
 import random
+import requests
 
 
 class Sensor:
@@ -43,7 +44,7 @@ class Sensor:
         event_types = ["nominal", "info", "warning", "error", "critical"]
         return random.choices(event_types, cum_weights=[60, 24, 10, 5, 1], k=1)[0]
 
-    def do_work(self):
+    async def do_work(self):
         time.sleep(random.uniform(0.1, 1.5))
 
     @property
@@ -60,8 +61,13 @@ class Sensor:
 
 def send_state(state):
     # TODO: Implement me
-    print(state)
-    pass
+    try:
+        result = requests.post("https://en6msadu8lecg.x.pipedream.net/", json=state, timeout=10)
+        print(result)
+        return True
+    except requests.RequestException as e:
+        print(e)
+        return False
 
 
 def main():
