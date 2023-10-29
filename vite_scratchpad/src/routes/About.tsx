e3180422bafc19f99374822600fbe3a33e484aca
Nicholas Ferrara <nferrara100@gmail.com>
2023-10-29 20:30:45 +0000

    feat: icon

--- vite_scratchpad/src/routes/About.tsx
+++ vite_scratchpad/src/routes/About.tsx
@@ -5,6 +5,13 @@ export function About() {
     <div>
       <h2>About</h2>
       <p>Learn more about us.</p>
+      <a target="_blank" href="https://icons8.com/icon/IDHst7MTjBqX/starburst-shape">
+        Starburst Shape
+      </a>{" "}
+      icon by{" "}
+      <a target="_blank" href="https://icons8.com">
+        Icons8
+      </a>
     </div>
   );
 }
