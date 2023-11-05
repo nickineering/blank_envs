691113124ed3776b2a32ad1440c87ffc5ad9c3df
Nicholas Ferrara <nferrara100@gmail.com>
2023-11-05 19:55:52 +0000

    test: typescript works with eslint

--- vite_scratchpad/.eslintrc.cjs
+++ vite_scratchpad/.eslintrc.cjs
@@ -1,20 +1,22 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2022: true },
   extends: [
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:react/jsx-runtime",
     "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/recommended",
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
   settings: { react: { version: "18.2" } },
-  plugins: ["react-refresh"],
+  plugins: ["react-refresh", "@typescript-eslint"],
   rules: {
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },
     ],
   },
+  parser: "@typescript-eslint/parser",
 };
