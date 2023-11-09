1f6845f96b9578f75c535adf829118f4178f850d
Nicholas Ferrara <nferrara100@gmail.com>
2023-11-09 12:44:16 +0000

    feat: mocking real request

--- python_scratchpad/src/__tests__/test_main.py
+++ python_scratchpad/src/__tests__/test_main.py
@@ -1,5 +1,13 @@
-from src.main import get_hello_text
+import requests_mock
+
+from src.main import get_api_response
 
 
-def test_get_hello_text():
-    assert get_hello_text() == "Hello, world!"
+def test_get_api_response():
+    with requests_mock.Mocker() as m:
+        url = "https://bloomberg-market-and-financial-news.p.rapidapi.com/market/auto-complete"
+        mock_response = {"hello": "world"}
+        m.get(url, json=mock_response)
+
+        result = get_api_response()
+        assert result == mock_response
--- python_scratchpad/src/main.py
+++ python_scratchpad/src/main.py
@@ -1,6 +1,21 @@
-def get_hello_text() -> str:
-    return "Hello, world!"
+import os
+
+from dotenv import load_dotenv
+from requests import get
+
+
+def get_api_response():
+    load_dotenv()
+
+    url = "https://bloomberg-market-and-financial-news.p.rapidapi.com/market/auto-complete"
+    querystring = {"query": "apple"}
+    headers = {
+        "X-RapidAPI-Key": os.environ["X-RAPIDAPI-KEY"],
+        "X-RapidAPI-Host": os.environ["X-RAPDIDAPI-HOST"],
+    }
+    response = get(url, headers=headers, params=querystring)
+    return response.json()
 
 
 if __name__ == "__main__":
-    print(get_hello_text())
+    print(get_api_response())
