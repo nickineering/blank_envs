69057cacdff94bfe5652de5d086da008d9349968
Nicholas Ferrara <nferrara100@gmail.com>
2023-11-10 22:26:40 +0000

    feat: python live coding interview

--- python_scratchpad/src/__tests__/test_main.py
+++ python_scratchpad/src/__tests__/test_main.py
@@ -1,13 +1,31 @@
-import requests_mock
+import os
 
-from src.main import get_api_response
+from src.main import get_name, main
+
+# from src.main import get_api_response
 
 
-def test_get_api_response():
-    with requests_mock.Mocker() as m:
-        url = "https://bloomberg-market-and-financial-news.p.rapidapi.com/market/auto-complete"
-        mock_response = {"hello": "world"}
-        m.get(url, json=mock_response)
+# def test_get_api_response():
+#     with requests_mock.Mocker() as m:
+#         url = "https://bloomberg-market-and-financial-news.p.rapidapi.com/market/auto-complete"
+#         mock_response = {"hello": "world"}
+#         m.get(url, json=mock_response)
 
-        result = get_api_response()
-        assert result == mock_response
+#         result = get_api_response()
+#         assert result == mock_response
+
+
+def test_name_returns_normal_length():
+    name = get_name("Short", "Name")
+    assert name == "Short Name"
+
+
+def test_name_returns_short_length():
+    name = get_name("VeryVeryVeryVeryVery", "LongName")
+    assert name == "V. LongName"
+
+
+def test_main_writes_to_filesystem():
+    main()
+    the_dir = os.listdir()
+    assert "output.csv" in the_dir
--- python_scratchpad/src/main.py
+++ python_scratchpad/src/main.py
@@ -1,21 +1,38 @@
-import os
+import csv
 
 from dotenv import load_dotenv
 from requests import get
 
 
-def get_api_response():
+def get_employees():
     load_dotenv()
 
-    url = "https://bloomberg-market-and-financial-news.p.rapidapi.com/market/auto-complete"
-    querystring = {"query": "apple"}
-    headers = {
-        "X-RapidAPI-Key": os.environ["X-RAPIDAPI-KEY"],
-        "X-RapidAPI-Host": os.environ["X-RAPDIDAPI-HOST"],
-    }
-    response = get(url, headers=headers, params=querystring)
-    return response.json()
+    url = "https://s3.eu-west-2.amazonaws.com/interview.thanskben.com/backend/employees.json"
+    response = get(url)
+    employees = response.json()
+    return employees
+
+
+def get_name(first_name, last_name):
+    result_first = first_name
+    length = len(f"{first_name} {last_name}")
+    if length > 15:
+        result_first = f"{first_name[0]}."
+    return f"{result_first} {last_name}"
+
+
+def main():
+    employees = get_employees()
+
+    with open("output.csv", "w", newline="\n") as csvfile:
+        output_writer = csv.writer(csvfile, delimiter=";")
+        output_writer.writerow(["email", "short full name"])
+
+        for employee in employees:
+            name = get_name(employee["first name"], employee["last name"])
+            line = [employee["email"], name]
+            output_writer.writerow(line)
 
 
 if __name__ == "__main__":
-    print(get_api_response())
+    main()
