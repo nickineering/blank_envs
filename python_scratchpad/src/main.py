e051182695a801bdda05464a44733c347acb3641
Nicholas Ferrara <nferrara100@gmail.com>
2024-03-26 15:59:18 +0000

    feat: get second smallest num without using sort

--- python_scratchpad/src/main.py
+++ python_scratchpad/src/main.py
@@ -1,9 +1,27 @@
-def get_second_small_num(nums: list[int]):
+import math
+
+
+def get_second_small_num_old(nums: list[int]):
     nums_set = set(nums)
     if len(nums_set) < 2:
         return None
     nums_result = sorted(nums_set)
     return nums_result[1]
+
+
+def get_second_small_num(nums: list[int]) -> float | int | None:
+    first = math.inf
+    second = math.inf
+    for num in nums:
+        if num < first:
+            second = first
+            first = num
+        elif num < second and num != first:
+            second = num
+
+    if second == math.inf:
+        second = None
+    return second
 
 
 if __name__ == "__main__":
